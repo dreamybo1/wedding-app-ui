2c82ce9c5784bb1d7a59ece06cca42d9ee5d02e5
Александр <dreamy@DreamyMac.local>
2026-05-25 16:30:24 +0500

    add zoom fix for ios

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -20,6 +20,20 @@ function App() {
     const handler = () => setScrollY(window.scrollY);
 
     window.addEventListener("scroll", handler, { passive: true });
+    // Запрет событий масштабирования 'gesturestart'
+    document.addEventListener("gesturestart", function (event) {
+      event.preventDefault();
+    });
+    /* Предотвращение масштабирования через 'touchmove' в старых версиях iOS */
+    document.addEventListener(
+      "touchmove",
+      function (event) {
+        if (event.scale !== 1) {
+          event.preventDefault();
+        }
+      },
+      { passive: false }
+    );
 
     return () => window.removeEventListener("scroll", handler);
   }, []);
